bcfe5580e85a1fc6549eacf2ec35b8acd2bb113f
aleshep <77703186+aleshep@users.noreply.github.com>
2021-02-25 20:35:26 -0500

    comments

--- labs/lab5/assign3/js/app.js
+++ labs/lab5/assign3/js/app.js
@@ -1,7 +1,9 @@
+//create canvas
 function setup() {
     createCanvas(600, 600);
 }
 
+//function draws 5 different bubbles, each with a different x and y position
 function draw() {
     background(230, 253, 255);
     bubble("#26cef0", mouseX, mouseY);
@@ -11,6 +13,8 @@ function draw() {
     bubble("#26cef0", mouseX/2, mouseY+100);
 }
 
+//creates the "bubbles" class
+//class includes the circle shape, fill, and stroke
 function bubble(bubbleColor, xHeight, yHeight) {
     
     fill(bubbleColor);
